764c5aeb53d742d6ce202c03b3e3f22bb2d1e4b8
Murilo Franceschetto <gfmurilogf@gmail.com>
2024-03-12 22:46:08 +0000

    Configurable port

--- app/main.js
+++ app/main.js
@@ -2,6 +2,7 @@ const net = require("net");
 const parseMessage = require("./parser");
 const commands = require("./commands/received-commands");
 
+
 function processMessage(data) {
   let parsedMessage = parseMessage(data);
   let commandToExecute = commands[parsedMessage[0]?.value?.toLowerCase()];
@@ -20,7 +21,14 @@ const server = net.createServer((connection) => {
   });
 }); 
 
-server.listen(6379, "127.0.0.1");
+let port = 6379;
+let indexPortConfig = process.argv.findIndex(arg => arg === '--port');
+const portConfig = Number(process.argv[indexPortConfig + 1]);
+if (indexPortConfig > -1 && portConfig) {
+  port = portConfig;
+}
+
+server.listen(port, "127.0.0.1", () => console.log(`Program running on port ${port}`));
 
 // console.log(processMessage('*5\r\n$3\r\nset\r\n$5\r\ngrape\r\n$4\r\npear\r\n$2\r\npx\r\n$4\r\n3000\r\n'));
 // console.log(processMessage('*2\r\n$3\r\nget\r\n$5\r\ngrape\r\n'));
